64dd1f4003a734f735bd6554aaa23331f24ea96a
imufiid <imufiid98@gmail.com>
2019-12-31 08:52:35 +0700

    added template home

---
+++ application/views/_templates_home/footer.php
@@ -0,0 +1,7 @@
+	<div class="kt-container mb-3" id="kt_footer">
+		<div class="kt-footer__wrapper">
+			<div class="kt-footer__copyright">
+				<span class="mr-2"><?php echo $_settings->setting_web_name ?></span> <?php echo date('Y') ?>&nbsp;&copy;&nbsp; <span class="ml-1"> support by:</span> <a href="<?php echo $_settings->setting_web_credit_href ?>" target="_blank" class="ml-2 kt-link"><?php echo $_settings->setting_web_credit ?></a>
+			</div>
+		</div>
+	</div>
