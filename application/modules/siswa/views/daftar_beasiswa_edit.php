47d90b33eea19a8ebabd581242a1f432cf945276
imufiid <imufiid98@gmail.com>
2019-12-31 08:51:43 +0700

    deleted

--- application/modules/siswa/views/daftar_beasiswa_edit.php
+++
@@ -1,7 +0,0 @@
-<?php $var = $this->db->where('siswa_beasiswa_id', $_GET['siswa_beasiswa_id'])->get('siswa_daftar_beasiswa')->row() ?>
-
-<div class="form-group">
-	<label>Nama</label>
-	<input type="text" autocomplete="off" value="<?php echo $var->siswa_beasiswa_label ?>" name="siswa_beasiswa_label" class="form-control">
-	<input type="hidden" autocomplete="off" name="siswa_beasiswa_id" value="<?php echo @$_GET['siswa_beasiswa_id'] ?>">
-</div>
--- application/modules/siswa/views/daftar_penghargaan_delete.php
+++
@@ -1,4 +0,0 @@
-<?php $var = $this->db->where('siswa_penghargaan_id', $_GET['siswa_penghargaan_id'])->get('siswa_daftar_penghargaan')->row() ?>
-
-<label><?php echo $var->siswa_penghargaan_label ?></label>
-<input type="hidden" autocomplete="off" name="siswa_penghargaan_id" value="<?php echo $var->siswa_penghargaan_id ?>">
