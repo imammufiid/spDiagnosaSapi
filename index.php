fd13b7364694a239974b177dee70e429bd84fb96
imufiid <imufiid98@gmail.com>
2020-01-06 21:52:08 +0700

    add lang datetime

--- index.php
+++ index.php
@@ -312,4 +312,6 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
+date_default_timezone_set("Asia/Jakarta");
+setlocale(LC_ALL, 'id_ID.UTF8', 'id_ID.UTF-8', 'id_ID.8859-1', 'id_ID', 'IND.UTF8', 'IND.UTF-8', 'IND.8859-1', 'IND', 'Indonesian.UTF8', 'Indonesian.UTF-8', 'Indonesian.8859-1', 'Indonesian', 'Indonesia', 'id', 'ID', 'en_US.UTF8', 'en_US.UTF-8', 'en_US.8859-1', 'en_US', 'American', 'ENG', 'English');
 require_once BASEPATH.'core/CodeIgniter.php';
